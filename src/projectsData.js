55bdbf0a0886802ea4fbfcfd9bf1532a9a9cb8a3
z12zt0 <evillyshlang@gmail.com>
2021-07-12 13:35:47 +0300

    Update projectsData.js

--- src/projectsData.js
+++ src/projectsData.js
@@ -1,42 +1,42 @@
 let projects = {
     survey: {
         link: "https://github.com/z12zt0/React-and-html-things/tree/master/Survey%20form",
-        img: ["./pics/survey_pic.PNG", "FCC's survey"], 
+        img: ["https://raw.githubusercontent.com/z12zt0/react-page/main/src/pics/survey_pic.PNG", "FCC's survey"], 
         caption: "Simple survey form"
     },
     product: {
         link: "https://github.com/z12zt0/React-and-html-things/tree/master/Product%20landing%20form",
-        img: ["https://github.com/z12zt0/personal_page-v2/blob/a5acb812fd0b0bc54d03063eaa78ee6583c2629b/pics_folder/buyPls_pic.PNG", "FCC's product page"], 
+        img: ["https://raw.githubusercontent.com/z12zt0/react-page/main/src/pics/buyPls_pic.PNG", "FCC's product page"], 
         caption: "Some product landing page"
     },
     technicalPage: {
         link: "https://github.com/z12zt0/React-and-html-things/tree/master/Technical%20page",
-        img: ["./src/Tech.PNG", "FCC's technical page"], 
+        img: ["https://raw.githubusercontent.com/z12zt0/react-page/main/src/pics/tech_pic.PNG", "FCC's technical page"], 
         caption: "Technical page"
     },
     rqm: {
         link: "https://codepen.io/z12zt0/full/zYozzWm",
-        img: ["./pics/rqm_pic.PNG", "FCC's random quote machine"], 
+        img: ["https://raw.githubusercontent.com/z12zt0/react-page/main/src/pics/rqm_pic.PNG", "FCC's random quote machine"], 
         caption: "Random quote machine"
     },
     calc: {
         link: "https://codepen.io/z12zt0/full/QWGaRva",
-        img: ["", "FCC's simple calculator"],
+        img: ["https://raw.githubusercontent.com/z12zt0/react-page/main/src/pics/calc_pic.PNG", "FCC's simple calculator"],
         caption: "Calculator"
     },
     drum: {
         link: "https://codepen.io/z12zt0/full/KKNXPZO",
-        img: ["", "FCC's drum machine"],
+        img: ["https://raw.githubusercontent.com/z12zt0/react-page/main/src/pics/drum_pic.PNG", "FCC's drum machine"],
         caption: "Drum machine"
     },
     markdown: {
         link: "https://codepen.io/z12zt0/full/jOVwYyv",
-        img: ["", "FCC's markdown testroom"],
+        img: ["https://raw.githubusercontent.com/z12zt0/react-page/main/src/pics/markdown_pic.PNG", "FCC's markdown testroom"],
         caption: "Markdown testroom"
     },
     pomodoroClock: {
         link: "https://codepen.io/z12zt0/full/BaQPEBR",
-        img: ["", "FCC's pomodoro clock (25 + 5)"],
+        img: ["https://raw.githubusercontent.com/z12zt0/react-page/main/src/pics/clock_pic.PNG", "FCC's pomodoro clock (25 + 5)"],
         caption: "Pomodoro clock (25 + 5)"
     }
 };
