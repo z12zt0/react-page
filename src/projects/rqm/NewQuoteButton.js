d9f40cc502b74e37f0e7ba9ea3e9f755092eae9f
z12zt0 <evillyshlang@gmail.com>
2021-11-15 22:18:51 +0300

    ??

--- src/projects/rqm/NewQuoteButton.js
+++
@@ -1,7 +0,0 @@
-const NewQuoteButton = ({handleClick=f=>f }) => {
-    return (
-        <button id="new-quote" onClick={() => handleClick()}>Get a new quote</button>
-    );
-};
-
-export default NewQuoteButton;
--- src/projects/rqm/Quotes.js
+++
@@ -1,9 +0,0 @@
-const Quotes = ({ quote }) => {
-    console.log("DATA IN QUOTES", quote)
-    const shortedQuote = quote.match(/^.+\./);
-    return (
-        <h3 id="text">{shortedQuote}</h3>
-    );
-};
-
-export default Quotes;
